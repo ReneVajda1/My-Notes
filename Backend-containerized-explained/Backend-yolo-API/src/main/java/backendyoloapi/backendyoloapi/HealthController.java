8e93f810a013d7094c1e055712e6da38d58e6400
ReneVajda1 <rene.vajda@yahoo.com>
2024-07-17 02:58:13 +0200

    health controller created

--- Backend-containerized-explained/Backend-yolo-API/src/main/java/backendyoloapi/backendyoloapi/HealthController.java
+++ Backend-containerized-explained/Backend-yolo-API/src/main/java/backendyoloapi/backendyoloapi/HealthController.java
@@ -1,2 +1,20 @@
-package backendyoloapi.backendyoloapi;public class HealthController {
+package backendyoloapi.backendyoloapi;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String renderHere(){
+        return "welcome traveler";
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK OK im alive");
+    }
+
 }
